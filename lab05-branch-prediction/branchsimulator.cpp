b8da9ee53194e97893d9bba57e88f3b148c88751
Tim Lu <hl4851@nyu.edu>
2023-11-29 14:56:59 -0500

    Add debug symbols to the branch prediction simulator

--- lab05-branch-prediction/branchsimulator.cpp
+++ lab05-branch-prediction/branchsimulator.cpp
@@ -1,10 +1,72 @@
-#include <math.h>
-
 #include <bitset>
+#include <cmath>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 
+#ifdef DEBUG
+#include "../debug.h"
+#else
+namespace debug {
+inline namespace bg {
+using ost = std::ostream&;
+ost reset(ost s) { return s; }
+ost black(ost s) { return s; }
+ost red(ost s) { return s; }
+ost green(ost s) { return s; }
+ost yellow(ost s) { return s; }
+ost blue(ost s) { return s; }
+ost magenta(ost s) { return s; }
+ost cyan(ost s) { return s; }
+ost white(ost s) { return s; }
+}  // namespace bg
+}  // namespace debug
+#endif
+
+inline namespace logging {
+
+struct debug_cout {};
+debug_cout dout;
+
+bool is_debug() { return nullptr != std::getenv("DEBUG"); }
+
+template < typename T >
+debug_cout& operator<<(debug_cout& s, [[maybe_unused]] const T& x) {
+#ifdef DEBUG
+    std::cout << x;
+#endif
+    return s;
+}
+
+debug_cout& operator<<(debug_cout& s,
+                       [[maybe_unused]] std::ostream& (*f)(std::ostream&)) {
+#ifdef DEBUG
+    f(std::cout);
+#endif
+    return s;
+}
+
+debug_cout& operator<<(debug_cout& s,
+                       [[maybe_unused]] std::ostream& (*f)(std::ios&)) {
+#ifdef DEBUG
+    f(std::cout);
+#endif
+    return s;
+}
+
+debug_cout& operator<<(debug_cout& s,
+                       [[maybe_unused]] std::ostream& (*f)(std::ios_base&)) {
+#ifdef DEBUG
+    f(std::cout);
+#endif
+    return s;
+}
+
+}  // namespace logging
+
+std::ios oldCoutState(nullptr);
 using namespace std;
 
 int main(int argc, char** argv) {
