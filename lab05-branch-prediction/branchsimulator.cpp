71d0a0a796c710d33282d87fa8e287295fea7944
Tim Lu <hl4851@nyu.edu>
2023-12-04 18:59:42 -0500

    Cleanup indexing logic

--- lab05-branch-prediction/branchsimulator.cpp
+++ lab05-branch-prediction/branchsimulator.cpp
@@ -24,64 +24,41 @@ struct Config {
     };
 };
 
-class BhtIndex {
-   public:
-    BhtIndex(unsigned addr_, Config cfg)
-        : addr((addr_ >> 2) && bitmask(cfg.h)){};
-
-    operator unsigned() { return addr; }
-
-    unsigned addr;
-};
-
 class BHT {
    public:
-    BHT(int w, int h)
-        : n_entries(pow(2, h)),
+    BHT(Config cfg_)
+        : cfg(cfg_),
+          n_entries(pow(2, cfg.h)),
           entries(n_entries, 0),
-          max_entry_val(pow(2, w) - 1) {}
+          max_entry_val(pow(2, cfg.w) - 1) {}
 
-    void update_state(bool branch_outcome, int bht_address) {
-        if ((size_t)bht_address > entries.size() - 1) {
-            cerr << "Invalid BHT address" << endl;
-            return;
-        }
-        int new_entry = entries[bht_address] << 1;
-        if (branch_outcome) {
-            new_entry |= 0b1;
-        }
-        new_entry &= max_entry_val;  // ensure entries don't exceed max val
-        entries[bht_address] = new_entry;
+    void update_state(unsigned bht_index, bool branch_outcome) {
+        unsigned new_entry = ((((*this)[bht_index] << 1) |
+                               static_cast< unsigned >(branch_outcome)) &
+                              bitmask(cfg.w));
+        entries[bht_index] = new_entry;
     }
 
-    int operator[](int index) {
+    unsigned operator[](int index) {
         if ((index < 0) || (index >= n_entries)) {
+            cout << "index: " << index << ", max_n: " << n_entries << endl;
             throw out_of_range("BHT [] index out of range");
         }
         return entries[index];
     }
 
     friend ostream& operator<<(ostream& os, const BHT& bht) {
-        for (size_t i = 0; i < bht.entries.size(); ++i) {
+        for (int i = 0; i < bht.n_entries; ++i) {
             cout << bht.entries[i] << endl;
         }
         return os;
     }
 
    private:
+    Config cfg;
     int n_entries;
-    vector< int > entries;
+    vector< unsigned > entries;
     int max_entry_val;
-};
-
-class PhtIndex {
-   public:
-    PhtIndex(unsigned pc, unsigned bht_entry, Config cfg)
-        : addr((pc >> 2) && bitmask(cfg.m - cfg.w)){};
-
-    operator unsigned() { return addr; }
-
-    unsigned addr;
 };
 
 class SaturatingCounter {
@@ -99,16 +76,11 @@ class SaturatingCounter {
 
 class PHT {
    public:
-    PHT(int m)
-        : n_entries(pow(2, m)), entries(n_entries, SaturatingCounter()) {}
+    PHT(Config cfg)
+        : n_entries(pow(2, cfg.m)), entries(n_entries, SaturatingCounter()) {}
 
-    void update_state(bool branch_outcome, int pht_address) {
-        if ((size_t)pht_address > entries.size() - 1) {
-            cerr << "Invalid PHT address" << endl;
-            return;
-        }
-
-        auto& counter = entries[pht_address];
+    void update_state(unsigned pht_index, bool branch_outcome) {
+        auto& counter = (*this)[pht_index];
         if (branch_outcome == 1) {
             counter.taken();
         } else {
@@ -116,15 +88,7 @@ class PHT {
         }
     }
 
-    bool make_prediction(int pht_address) {
-        if (entries[pht_address].state < 2) {  // not taken
-            return 0;
-        } else {
-            return 1;
-        }
-    }
-
-    auto operator[](int index) {
+    SaturatingCounter& operator[](int index) {
         if ((index < 0) || (index >= n_entries)) {
             throw out_of_range("PHT [] index out of range");
         }
@@ -132,7 +96,7 @@ class PHT {
     }
 
     friend ostream& operator<<(ostream& os, const PHT& pht) {
-        for (size_t i = 0; i < pht.entries.size(); ++i) {
+        for (int i = 0; i < pht.n_entries; ++i) {
             cout << pht.entries[i].state << endl;
         }
         return os;
@@ -140,7 +104,7 @@ class PHT {
 
    private:
     int n_entries;
-    vector<SaturatingCounter> entries;
+    vector< SaturatingCounter > entries;
 };
 
 int main([[maybe_unused]] int argc, char** argv) {
@@ -151,53 +115,48 @@ int main([[maybe_unused]] int argc, char** argv) {
         cout << cfg;
     }
 
-    PHT my_pht(m);
-    // cout << "The PHT looks like this when initialized: \n" << my_pht << endl;
-    // my_pht.update_state(1, 0);
-    // my_pht.update_state(1, 1);
-    // my_pht.update_state(1, 1);
-    // my_pht.update_state(0, 2);
-    // my_pht.update_state(0, 3);
-    // my_pht.update_state(0, 3);
-    // my_pht.update_state(0, 4);
-    // my_pht.update_state(1, 4);
-    // cout << "PHT state machine test: \n" << my_pht << endl;
+    auto BhtIndex = [cfg](unsigned pc) -> unsigned {
+        return pc >> 2 & bitmask(cfg.h);
+    };
 
-    BHT my_bht(w, h);
-    // cout << "The BHT looks like this when initialized: \n" << my_bht << endl;
-    // my_bht.update_state(1, 0);
-    // my_bht.update_state(0, 0);
-    // my_bht.update_state(1, 0);
-    // my_bht.update_state(0, 0);
-    // my_bht.update_state(1, 3);
-    // cout << "BHT update test: \n" << my_bht << endl;
+    auto PhtIndex = [cfg](unsigned pc, unsigned bht_entry) -> unsigned {
+        return (pc >> 2 & bitmask(cfg.m - cfg.w)) << cfg.w |
+               (bht_entry & bitmask(cfg.w));
+    };
+
+    PHT pht(cfg);
+    BHT bht(cfg);
 
     ofstream out((string(argv[2]) + ".out").c_str());
     ifstream trace(argv[2]);
-    int address;
-    int branch_outcome;
+
     int count = 0;
+    unsigned address, branch_outcome;
     while (trace >> hex >> address >> dec >> branch_outcome) {
-        cout << "addr: 0x" << hex << address;
-        cout << " - branch: " << dec << branch_outcome << endl;
+        cout << "--------------------------------" << endl;
+        cout << "  " << ++count << endl;
+        cout << "--------------------------------" << endl;
+        cout << "addr: 0x" << hex << address << dec << endl;
 
-        int bht_address = (address >> 2) & (int)(pow(2, h) - 1);
+        const unsigned bht_index = BhtIndex(address);
+        const unsigned pht_index = PhtIndex(address, bht[bht_index]);
+        cout << "bht_index: " << bht_index << endl;
+        cout << "pht_index: " << pht_index << endl;
 
-        int pht_addr_upper = (address >> 2) & (int)(pow(2, m - w) - 1);
-        int pht_addr_lower = my_bht[bht_address];
-        int pht_address = (pht_addr_upper << w) | pht_addr_lower;
-        out << my_pht.make_prediction(pht_address) << endl;  // make prediction
+        const bool prediction = pht[pht_index].predict();
+        out << prediction << endl;
 
-        my_pht.update_state(branch_outcome, pht_address);
-        my_bht.update_state(branch_outcome, bht_address);
+        cout << "BHT entry (before): " << bitset< 32 >(bht[bht_index]) << endl;
+        cout << "PHT entry (before): " << pht[pht_index].state << endl;
 
-        ++count;
-        cout << "BHT after update: " << count << '\n' << my_bht << endl;
-        cout << "PHT after update: " << count << '\n' << my_pht << endl;
+        cout << "predict: " << (prediction ? "taken" : "not taken") << endl;
+        cout << "branch:  " << (branch_outcome ? "TAKEN" : "NOT TAKEN") << endl;
 
-        // cout << "Address: " << address << ' ';
-        // cout << "Branch Outcome: " << branch_outcome << endl;
-        // out << 0 << endl; // predict not taken
+        pht.update_state(pht_index, branch_outcome);
+        bht.update_state(bht_index, branch_outcome);
+
+        cout << "BHT entry (after):  " << bitset< 32 >(bht[bht_index]) << endl;
+        cout << "PHT entry (after):  " << pht[pht_index].state << endl;
     }
 
     trace.close();
